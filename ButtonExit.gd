a16466b13745d3ec6f053c961d28d4a861caf05f
eXCore <excoreky@gmail.com>
2020-05-21 23:31:52 +0300

    Exit button

---
+++ ButtonExit.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _ready():
+	pass
+
+
+func _on_ButtonExit_pressed():
+	get_tree().quit()
--- Menu.gd
+++ Menu.gd
@@ -1,33 +1,12 @@
-extends Node2D
-
-var firstDelay = true
+extends Control
 
 func _ready():
-	if firstDelay:
-		firstDelay = false
-		$WorldBorder/PlayerArea.count_down()
-	var _con = get_viewport().connect("size_changed", self, "_on_resize")
-	$BackgroundSprite.scale = Vector2(0.42*GlobalVars.currentResolution[0]/1280, 0.42*GlobalVars.currentResolution[0]/1280)
-	$BackgroundSprite.position = GlobalVars.currentResolution / 2
 	pass
-	
-	
+
+
 func _input(_event):
 	if Input.is_action_just_pressed("ui_fullscreen"):
 		call_deferred("toggle_fullscreen")
-	elif Input.is_action_just_pressed("ui_cancel"):
-		var popup = AcceptDialog.new()
-		popup.dialog_text = "Exit to main menu?"
-		popup.window_title = ""
-		add_child(popup)
-		popup.rect_position = GlobalVars.currentResolution / 2 - Vector2(50, 30)
-		popup.show()
-		popup.connect("confirmed", self, "_on_exit_confirm")
-		
-
-func _on_exit_confirm():
-	get_tree().change_scene("Menu.tscn")
-	
 
 func toggle_fullscreen():
 	if !OS.window_fullscreen:
@@ -40,6 +19,7 @@ func toggle_fullscreen():
 	OS.window_size = GlobalVars.currentResolution
 	print(OS.window_size)
 	call_deferred("_on_resize")
+
 
 func _on_resize():
 	if OS.window_size[0] < GlobalVars.minimumResolution[0]:
